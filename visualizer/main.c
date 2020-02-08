93183045b7f15d7d83dd94379308a6b55b71f34d
NktTrpv <nikita_toropov@nkt.local>
2020-02-09 01:04:49 +0300

    colorfull visualizer

--- visualizer/main.c
+++ visualizer/main.c
@@ -3,7 +3,18 @@
 #include <unistd.h>
 
 # define X_CONST		4
+# define RESET			"\033[0m"
+# define RED			"\033[1;31m"
+# define PINK			"\033[1;39m"
+# define BLUE			"\033[1;35m"
+# define ANOTHER		"\033[34m"
 
+void		print_color_char(char c, char *color)
+{
+	write(1, color, ft_strlen(color));
+	write(1, &c, 1);
+	write(1, RESET, ft_strlen(RESET));
+}
 int			main(void)
 {
 	char	*line;
@@ -21,14 +32,22 @@ int			main(void)
 			write(1, "\n", 1);
 			free(line);
 			i = 0;
-			// while (get_next_line(0, &line) && ft_atoi(line) == i)
 			while (get_next_line(0, &line) && ft_isdigit(line[0]) &&
 			ft_isdigit(line[1]) && ft_isdigit(line[2]) && line[3] == ' ')
 			{
 				j = 0;
 				while (line[j])
 				{
-					write(1, &line[j], 1);
+					if (line[j] == 'O')
+						print_color_char(line[j], RED);
+					else if (line[j] == 'o')
+						print_color_char(line[j], PINK);
+					else if (line[j] == 'X')
+						print_color_char(line[j], BLUE);
+					else if (line[j] == 'x')
+						print_color_char(line[j], ANOTHER);
+					else
+						write(1, &line[j], 1);
 					j++;
 				}
 				free(line);
@@ -36,26 +55,18 @@ int			main(void)
 				i++;
 			}
 		}
-		usleep(10000);
-
+		// if (get_next_line(0, line) && ft_strncmp(line, "== O fin:", 8))
+		// {
+		// 	write(1, line, ft_strlen(line));
+		// 	write(1, "\n", 1);
+		// 	free(line);
+		// 	get_next_line(0, line);
+		// 	write(1, line, ft_strlen(line));
+		// 	write(1, "\n", 1);
+			
+		// }
+		usleep(100000);
 		free(line);
 	}
-	// while (get_next_line(0, &line) && ft_atoi(line) == i)
-	// {
-	// 	j = X_CONST;
-	// 	while (line[j])
-	// 	{
-	// 		write(1, &line[j], 1);
-	// 		// if (line[j] == o)
-	// 		// {
-	// 		// 	write(1, "", );
-	// 		// }
-	// 		j++;
-	// 	}
-	// 	write(1, "\n", 1);
-	// 	usleep(10000);
-	// 	system("clear");
-	// 	i++;
-	// }
 	return (0);
 }
