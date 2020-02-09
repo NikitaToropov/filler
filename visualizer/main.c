e89186216f674556b6e429ef886304c0855f6e3a
Clement Missy <cmissy@at-g4.21-school.ru>
2020-02-09 20:39:55 +0300

    nice colors but need output name of winner and looser at the end

--- visualizer/main.c
+++ visualizer/main.c
@@ -5,10 +5,9 @@
 # define X_CONST		4
 # define RESET			"\033[0m"
 # define RED			"\033[1;31m"
-# define PINK			"\033[1;39m"
-# define BLUE			"\033[1;35m"
-# define ANOTHER			"\033[1;39m"
-// # define ANOTHER		"\033[34m"
+# define PINK			"\033[1;35m"
+# define BLUE			"\033[1;34m"
+# define CYAN			"\033[1;36m"
 
 void		print_color_char(char c, char *color)
 {
@@ -16,6 +15,21 @@ void		print_color_char(char c, char *color)
 	write(1, &c, 1);
 	write(1, RESET, ft_strlen(RESET));
 }
+
+void		take_correct_collor(char c)
+{
+	if (c == 'O')
+		print_color_char(c, RED);
+	else if (c == 'o')
+		print_color_char(c, PINK);
+	else if (c == 'X')
+		print_color_char(c, BLUE);
+	else if (c == 'x')
+		print_color_char(c, CYAN);
+	else
+		write(1, &c, 1);
+}
+
 int			main(void)
 {
 	char	*line;
@@ -33,26 +47,12 @@ int			main(void)
 			write(1, "\n", 1);
 			free(line);
 			i = 0;
-			// while (get_next_line(0, &line) && ft_isdigit(line[0]) &&
-			// ft_isdigit(line[1]) && ft_isdigit(line[2]) && line[3] == ' ')
 			while (get_next_line(0, &line) && line[3] == ' ' &&
 			ft_atoi(line) == i)
 			{
 				j = 0;
 				while (line[j])
-				{
-					if (line[j] == 'O')
-						print_color_char(line[j], RED);
-					else if (line[j] == 'o')
-						print_color_char(line[j], PINK);
-					else if (line[j] == 'X')
-						print_color_char(line[j], BLUE);
-					else if (line[j] == 'x')
-						print_color_char(line[j], ANOTHER);
-					else
-						write(1, &line[j], 1);
-					j++;
-				}
+					take_correct_collor(line[j++]);
 				free(line);
 				write(1, "\n", 1);
 				i++;
